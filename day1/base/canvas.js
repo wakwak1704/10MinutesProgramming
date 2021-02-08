5928e7a37183c510a18535e291dfca4b87ee32e2
wakwak1704 <ryoya.second@gmail.com>
2021-02-08 23:27:03 +0900

    update day1

--- day1/base/canvas.js
+++ day1/base/canvas.js
@@ -1,3 +1,10 @@
 window.onload = function () {
+    /* sample */
+    const cvs = document.getElementById("my-canvas");
+    const ctx = cvs.getContext("2d");
+    ctx.fillStyle = "gray";
+    ctx.fillRect(50, 50, 200, 100);
+    
     /* implement here */
+    
 }
