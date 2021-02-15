ac86f682a9a337912b8a4b0494c32341dc0caaf8
wakwak1704 <ryoya.second@gmail.com>
2021-02-16 00:25:19 +0900

    add day5

---
+++ day5/goal/identicon.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('#generate').on('click', function() {
+        const userName = $('#user-name').val();   
+        console.log('user name: ', userName);     
+        const hash = md5(userName);
+        console.log('hash: ', hash);
+    })
+});
