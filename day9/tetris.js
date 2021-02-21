0de59eb0705ac078f3fb62e7165a6380167b7815
wakwak1704 <ryoya.second@gmail.com>
2021-02-21 21:24:47 +0900

    study

---
+++ day9/tetris.js
@@ -0,0 +1,23 @@
+const HEIGHT = 19;
+const WIDTH = 10;
+
+$(function() {
+
+    let fixedBlocks = new Arra(HEIGHT * WIDTH).fill(false);
+
+});
+
+function generatePiece() {
+    
+    for(let i = 0; i < )
+    let p = Math.random(1);
+}
+function (obj) {
+    if(obj.id == 'start-btn') {
+        $('#start-btn').css('display', 'none');
+        $('#reset-btn').css('display', 'block');
+    } else if(obj.id = 'reset-btn') {
+        $('#start-btn').css('display', 'block');
+        $('#reset-btn').css('display', 'none');
+    }
+}
