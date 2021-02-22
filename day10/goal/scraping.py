406317d572a73ba1abc91082f4ab74fe99293727
wakwak1704 <ryoya.second@gmail.com>
2021-02-22 23:35:21 +0900

    update day10

--- day10/goal/scraping.py
+++ day10/goal/scraping.py
@@ -10,15 +10,4 @@ html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
 # classに"ingredient_row"を含むdiv要素を取得
 ingredients = soup.select("div[class='ingredient_row']")
-# CSVファイルをオープン（新規作成）
-with open("ingredient.csv", "w") as f:
-    writer = csv.writer(f)
-    # 材料の数だけループ
-    for ingredient in ingredients:
-        # 材料名と量のテキストを取得
-        name = ingredient.select("div[class*='ingredient_name']")[0].text.strip()
-        quantity = ingredient.select("div[class*='ingredient_quantity']")[0].text.strip()
-        # 標準出力
-        print(name, quantity)
-        # CSVに書き込み
-        writer.writerow([name, quantity])
+print(ingredients)
