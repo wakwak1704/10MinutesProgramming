c34b9c5fef9510d4b960280373a25f5b5ca0e4d2
wakwak1704 <ryoya.second@gmail.com>
2021-03-06 19:57:56 +0900

    play toggl api from python

--- day14/sample/toggl.py
+++ day14/sample/toggl.py
@@ -1,10 +1,32 @@
 import base64
 import urllib3
+import json
+import math
 
-url = "http://sample.com/index.html"
-token = "xxx"
-headers ={}
-headers["authorization"] = "Basic " + base64.b64encode(token.encode()).decode('utf-8')
-http = urllib3.PoolManager()
-req = http.request(method="get", url=url, headers=headers)
-print(req.data)
+
+def getWorkDescription():
+    url = "https://api.track.toggl.com/api/v8/time_entries"
+    fields = {"start_date": "2021-02-01T12:00:00+02:00",
+              "end_date": "2021-03-01T12:00:00+02:00"}
+    token = "xxx"
+    headers = urllib3.util.make_headers(basic_auth=token + ':api_token')
+    http = urllib3.PoolManager()
+    req = http.request(method="get", url=url, fields=fields, headers=headers)
+    entries = json.loads(req.data)
+    result = ""
+    isFirstLine = True
+    for entry in entries:
+        if(not isFirstLine):
+            result = result + "\n"
+        else:
+            isFirstLine = False
+        result = result + entry["description"]
+        result = result + " ("
+        result = result + (str(math.ceil(entry["duration"] /
+                                         (60 * 60 * 60) * 10) / 10))
+        result = result + ("時間")
+        result = result + ")"
+    return result
+
+
+print(getWorkDescription())
