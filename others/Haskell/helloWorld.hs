de83ea17f79810effd2d9f4805058f5e1d1ae885
wakwak1704 <ryoya.second@gmail.com>
2021-03-12 23:51:16 +0900

    play Haskell

--- others/Haskell/helloWorld.hs
+++ others/Haskell/helloWorld.hs
@@ -1,7 +1,11 @@
-main = do
+-- main = do
     -- putStrLn "input line"
     -- line <- getLine
     -- putStrLn line
-    putStrLn "input content"
-    content <- getContents
-    putStrLn content
+    -- putStrLn "input content"
+    -- content <- getContents
+    -- putStrLn content
+inc x = x + 1
+
+main = do
+    print $ inc 5
