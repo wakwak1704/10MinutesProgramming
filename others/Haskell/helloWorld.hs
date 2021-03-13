68a7e1d6ddc68a68cf12f1b15a5cb3eac3668992
wakwak1704 <ryoya.second@gmail.com>
2021-03-13 22:24:24 +0900

    play Haskell

--- others/Haskell/helloWorld.hs
+++ others/Haskell/helloWorld.hs
@@ -5,7 +5,15 @@
     -- putStrLn "input content"
     -- content <- getContents
     -- putStrLn content
-inc x = x + 1
+-- inc x = x + 1
 
-main = do
-    print $ inc 5
+-- main = do
+--     print $ inc 5
+
+listA = [3, 2, 4]
+listB = [3, 3, 2]
+
+result = if listA < listB then "list B is begger than list A"
+else "list B is not begger than list A"
+
+main = putStrLn result
